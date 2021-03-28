77e9871da5ab6d9872e4d8eceeccbcad29fbb77d
Neal Maze <nealwmaze@gmail.com>
2021-03-27 20:41:44 -0600

    fixed average change value

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,7 @@ import statistics as st
 csvFile = open('resources/budget_data.csv')
 reader = csv.reader(csvFile, delimiter=',')
 data = list(reader)
+
 data.pop(0)
 previousCoffers = int(data[1][1])
 totalMonths = 0
@@ -41,13 +42,12 @@ for line in data:
         decreaseMonth = line[0]
     previousCoffers = int(line[1])
 
+changeList.pop(0)
 averageChange = st.mean(changeList)
+averageChange = round(averageChange, 2)
 
 # double checking the average change result because the number does not match the example given
-total = 0
-for item in changeList:
-    total = total + 1
-sumChanges = sum(changeList)
+
 
 textOne = "Financial Analysis" + "\n" + "- - - - - - - - - - - - - - - - - - - - - - - - "
 textTwo = "Total Months: " + str(totalMonths)
